55660d0faff218646bf6838d6a9270e9f7ea2048
Tom Renicker <trrenicker@gmail.com>
2017-08-06 19:45:21 -0400

    Initial Commit

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+	id INTEGER(11) NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN NOT NULL,
+	date TIMESTAMP,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (
+	burger_name,
+	devoured
+) 
+VALUES 
+('Hickory BBQ Burger', 0),
+('Southwest Black Bean Burger', 0),
+('Turkey Burger', 0);
