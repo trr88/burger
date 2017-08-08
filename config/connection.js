63a19f846b8c6284968ce69a571ef20c03be4f9c
Tom Renicker <trrenicker@gmail.com>
2017-08-08 19:05:24 -0400

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    port: 3306,
+    port: process.env.PORT || 3306,
     host: "127.0.0.1",
     user: "root",
     password: "1208Dawn!",
