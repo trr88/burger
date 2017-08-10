f8ce1e4565235d6770eda3f3e7006f602734a035
Tom Renicker <trrenicker@gmail.com>
2017-08-10 18:26:48 -0400

    changed Connection info

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
 var mysql = require("mysql");
 
+
+//mysql://b4f3f4acf31113:e549cc1f@us-cdbr-iron-east-05.cleardb.net/heroku_4f175b3477c2071?reconnect=true
 var connection = mysql.createConnection({
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user: 'b4f3f4acf31113',
+    password: 'e549cc1f',
+    database: 'heroku_4f175b3477c2071'
+});
+
+/*var connection = mysql.createConnection({
     port: process.env.PORT || 3306,
     host: "127.0.0.1",
     user: "root",
     password: "1208Dawn!",
     database: "burgers_db"
-});
+});*/
 
 connection.connect(function(err) {
     if (err) {
